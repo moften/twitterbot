39095e648c7fcddc3a806eb53ebab9d9a46127e6
moften <ing.fco.ls@gmail.com>
2015-07-07 20:16:31 -0500

    07072015

---
+++ AsyncTwitterBot/WorkerRole.cs
@@ -0,0 +1,135 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using TweetSharp;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AsyncTwitterBot
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private string API_key = "";
+        private string API_secret = "";
+        private string Access_token_secret = "";
+        private string Access_token = "";
+    
+        private readonly List<TwitterSearchRow> _initialQueries = new List<TwitterSearchRow>
+        {
+            new TwitterSearchRow("#publicidad"),
+           new TwitterSearchRow("#marketing"),
+           new TwitterSearchRow("#marketingdigital"),
+           new TwitterSearchRow("#redessociales"),
+           new TwitterSearchRow("#socialmedia"),
+           new TwitterSearchRow("#marketingonline"),
+
+        };
+
+        private const string PartitionKey = "PK";
+        private const string TableName = "metadata";
+
+        private List<TwitterSearchRow> Queries { get; set; }
+
+        public CloudTable MetadataTable { get; set; }
+        public TwitterService TwitterServiceInstance { get; set; }
+
+        public override void Run()
+        {
+            // In v1.1, all API calls require authentication
+            TwitterServiceInstance = new TwitterService(API_key, API_secret);
+            TwitterServiceInstance.AuthenticateWith(Access_token, Access_token_secret);
+            
+            Queries = GetQueries();
+
+            
+            while (true)
+            {
+                Thread.Sleep(15 * 60 * 1000);
+                
+                foreach (var query in Queries)
+                {
+                    try
+                    {
+                        ProcessQuery(query);
+                        Trace.TraceError("OK bro: " + query.RowKey);
+                    }
+                    catch
+                    {
+                        Trace.TraceError("ER bro: " + query.RowKey);
+                    }
+                }
+
+                PersistQueryTable(Queries);
+            }
+        }
+
+        private List<TwitterSearchRow> GetQueries()
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(TableName);
+            MetadataTable = table;
+
+            if (!table.Exists())
+            {
+                table.CreateIfNotExists();
+                return _initialQueries;
+            }
+
+            var query = new TableQuery<TwitterSearchRow>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, PartitionKey));
+            return table.ExecuteQuery(query).ToList();
+        }
+
+        public void ProcessQuery(TwitterSearchRow query)
+        {
+            var searchResults = TwitterServiceInstance.Search(new SearchOptions
+            {
+                Count = 9, //TODO: este count tiene que ser 15 dividido la cantidad de queries
+                Q = query.SearchTerm,
+                SinceId = query.LastQueryId,
+                Resulttype = TwitterSearchResultType.Popular,
+            });
+
+            if (!searchResults.Statuses.Any()) return;
+
+            foreach (var tweet in searchResults.Statuses)
+            {
+                TwitterServiceInstance.FavoriteTweet(new FavoriteTweetOptions { Id = tweet.Id });
+                query.FavouritedTW++;
+            }
+
+            query.LastQueryId = searchResults.Statuses.Max(x => x.Id);
+        }
+        
+        private void PersistQueryTable(IEnumerable<TwitterSearchRow> queries)
+        {
+            var batchOperation = new TableBatchOperation();
+            foreach(var query in queries)
+            {
+                batchOperation.InsertOrReplace(query);
+            }
+            MetadataTable.ExecuteBatch(batchOperation);
+        }
+    }
+
+    public class TwitterSearchRow : TableEntity
+    {
+        public TwitterSearchRow(string searchTerm)
+        {
+            PartitionKey = "PK";
+            RowKey = searchTerm.Replace("#","");
+            SearchTerm = searchTerm;
+        }
+
+        public string SearchTerm { get; set; }
+
+        public TwitterSearchRow() { }
+
+        public long LastQueryId { get; set; }
+
+        public int FavouritedTW { get; set; }
+    }
+}
---
+++ TwitterBot/TwitterBot/csx/Release/ServiceDefinition.rd
@@ -0,0 +1,66 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<serviceModel xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" name="TwitterBot" generation="1" functional="0" release="0" Id="298f9a2e-34b2-4edb-bf81-76673090f138" dslVersion="1.2.0.0" xmlns="http://schemas.microsoft.com/dsltools/RDSM">
+  <groups>
+    <group name="TwitterBotGroup" generation="1" functional="0" release="0">
+      <settings>
+        <aCS name="AsyncTwitterBot:Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString" defaultValue="">
+          <maps>
+            <mapMoniker name="/TwitterBot/TwitterBotGroup/MapAsyncTwitterBot:Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString" />
+          </maps>
+        </aCS>
+        <aCS name="AsyncTwitterBot:StorageConnectionString" defaultValue="">
+          <maps>
+            <mapMoniker name="/TwitterBot/TwitterBotGroup/MapAsyncTwitterBot:StorageConnectionString" />
+          </maps>
+        </aCS>
+        <aCS name="AsyncTwitterBotInstances" defaultValue="[1,1,1]">
+          <maps>
+            <mapMoniker name="/TwitterBot/TwitterBotGroup/MapAsyncTwitterBotInstances" />
+          </maps>
+        </aCS>
+      </settings>
+      <maps>
+        <map name="MapAsyncTwitterBot:Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString" kind="Identity">
+          <setting>
+            <aCSMoniker name="/TwitterBot/TwitterBotGroup/AsyncTwitterBot/Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString" />
+          </setting>
+        </map>
+        <map name="MapAsyncTwitterBot:StorageConnectionString" kind="Identity">
+          <setting>
+            <aCSMoniker name="/TwitterBot/TwitterBotGroup/AsyncTwitterBot/StorageConnectionString" />
+          </setting>
+        </map>
+        <map name="MapAsyncTwitterBotInstances" kind="Identity">
+          <setting>
+            <sCSPolicyIDMoniker name="/TwitterBot/TwitterBotGroup/AsyncTwitterBotInstances" />
+          </setting>
+        </map>
+      </maps>
+      <components>
+        <groupHascomponents>
+          <role name="AsyncTwitterBot" generation="1" functional="0" release="0" software="C:\Users\FranciscoJavier\Downloads\TwitterBot\TwitterBot\TwitterBot\csx\Release\roles\AsyncTwitterBot" entryPoint="base\x64\WaHostBootstrapper.exe" parameters="base\x64\WaWorkerHost.exe " memIndex="-1" hostingEnvironment="consoleroleadmin" hostingEnvironmentVersion="2">
+            <settings>
+              <aCS name="Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString" defaultValue="" />
+              <aCS name="StorageConnectionString" defaultValue="" />
+              <aCS name="__ModelData" defaultValue="&lt;m role=&quot;AsyncTwitterBot&quot; xmlns=&quot;urn:azure:m:v1&quot;&gt;&lt;r name=&quot;AsyncTwitterBot&quot; /&gt;&lt;/m&gt;" />
+            </settings>
+            <resourcereferences>
+              <resourceReference name="DiagnosticStore" defaultAmount="[4096,4096,4096]" defaultSticky="true" kind="Directory" />
+              <resourceReference name="EventStore" defaultAmount="[1000,1000,1000]" defaultSticky="false" kind="LogStore" />
+            </resourcereferences>
+          </role>
+          <sCSPolicy>
+            <sCSPolicyIDMoniker name="/TwitterBot/TwitterBotGroup/AsyncTwitterBotInstances" />
+            <sCSPolicyUpdateDomainMoniker name="/TwitterBot/TwitterBotGroup/AsyncTwitterBotUpgradeDomains" />
+            <sCSPolicyFaultDomainMoniker name="/TwitterBot/TwitterBotGroup/AsyncTwitterBotFaultDomains" />
+          </sCSPolicy>
+        </groupHascomponents>
+      </components>
+      <sCSPolicy>
+        <sCSPolicyUpdateDomain name="AsyncTwitterBotUpgradeDomains" defaultPolicy="[5,5,5]" />
+        <sCSPolicyFaultDomain name="AsyncTwitterBotFaultDomains" defaultPolicy="[2,2,2]" />
+        <sCSPolicyID name="AsyncTwitterBotInstances" defaultPolicy="[1,1,1]" />
+      </sCSPolicy>
+    </group>
+  </groups>
+</serviceModel>
